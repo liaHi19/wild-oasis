d251b054f1416dda58a52d9a17b83a0a0a186dc5
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-11-08 14:30:56 +0000

    feat: adding layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1rem;
+  grid-template-rows: auto 1fr;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-0);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
